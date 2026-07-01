4f4706da1113f9f18d4359a784d8d2d772707cff
Zafer Balkan <zafer@zaferbalkan.com>
2026-07-01 20:53:54 +0300

    Added int for the easter egg
    Signed-off-by: Zafer Balkan <zafer@zaferbalkan.com>

--- assets/js/konami.js
+++ assets/js/konami.js
@@ -1,4 +1,9 @@
 (function () {
+  console.log(
+    "%c👾 Psst... looking for a secret? Try this: ⬆️ ⬆️ ⬇️ ⬇️ ⬅️ ➡️ ⬅️ ➡️ 🅱️ 🅰️",
+    "color: #00ff00; font-family: monospace; font-size: 14px; font-weight: bold; text-shadow: 1px 1px 2px black;"
+  );
+
   if (window.location.pathname === "/game/" || window.location.pathname === "/game/index.html") {
     return;
   }
